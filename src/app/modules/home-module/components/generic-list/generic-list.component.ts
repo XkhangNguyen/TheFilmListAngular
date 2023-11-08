283cf2dd5e6449b85f223108b221ee736be4c0b8
xkhang <xkhang.nguyen2bme@gmail.com>
2023-11-09 01:34:16 +0700

    Add styles for carousel nav buttons

--- src/app/modules/home-module/components/generic-list/generic-list.component.ts
+++ src/app/modules/home-module/components/generic-list/generic-list.component.ts
@@ -1,21 +1,40 @@
-import { Component, OnInit } from '@angular/core';
-import { Movie } from 'src/app/core/models/movie';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { TmdbService } from 'src/app/core/services/TMDB/tmdb.service';
 import { OwlOptions } from 'ngx-owl-carousel-o';
 
 @Component({
   selector: 'app-generic-list',
   templateUrl: './generic-list.component.html',
-  styleUrls: ['./generic-list.component.scss']
+  styleUrls: ['./generic-list.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class GenericListComponent implements OnInit {
   movies: any;
 
   carouselOptions: OwlOptions = {
     loop: true,
-    mouseDrag: true,
-    items: 10,
+    mouseDrag: false,
+    touchDrag: false,
+    pullDrag: false,
+    autoplay: true,
+    responsive: {
+      0: {
+        items: 1
+      },
+      400: {
+        items: 3
+      },
+      740: {
+        items: 5
+      },
+      940: {
+        items: 10
+      }
+    },
     margin: 10,
+    nav: true,
+    navText: ['&#8249', '&#8250;'],
+    dots: false,
   }
   
   constructor(private tmdbService: TmdbService) {}
