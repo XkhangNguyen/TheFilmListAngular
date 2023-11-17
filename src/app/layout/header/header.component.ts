9cc0592b86b08e5c317f4c6eb0c3bc729619cd8a
Khang <xkhang.nguyen2bme@gmail.com>
2023-11-17 23:42:30 +0700

    Fix sticky header

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -7,23 +7,27 @@ import { Component, ElementRef, HostListener, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
   isSticky: boolean = false;
-  topValue!: number;
+  minHeightValue!: number;
 
   constructor(private el: ElementRef) {}
 
   @HostListener('window:scroll', ['$event'])
   checkScroll() {
-    this.isSticky = window.pageYOffset >= this.topValue;
+    this.isSticky =
+      0.1 * window.innerHeight > this.minHeightValue
+        ? window.scrollY >= 0.9 * window.innerHeight
+        : window.scrollY >= window.innerHeight - this.minHeightValue;
   }
 
   ngOnInit() {
+    console.log(this.getMinHeight());
+    console.log(0.1 * window.innerHeight);
+    this.minHeightValue = this.getMinHeight();
     this.checkScroll();
-    this.topValue = parseFloat(this.getStyle('top'));
   }
 
-  private getStyle(property: string): string {
-    return getComputedStyle(this.el.nativeElement.firstChild).getPropertyValue(
-      property
-    );
+  private getMinHeight(): number {
+    const computedStyle = getComputedStyle(this.el.nativeElement.firstChild);
+    return parseFloat(computedStyle.minHeight.replace('px', ''));
   }
 }
